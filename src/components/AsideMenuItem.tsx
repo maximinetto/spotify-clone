9a198d6fa2b363d1e5052953b9dd5eaa60a91f6a
Maximiliano Minetto <maximinetto@gmail.com>
2024-01-01 22:21:24 -0300

    Init next section

---
+++ src/components/AsideMenuItem.tsx
@@ -0,0 +1,72 @@
+import clsx from "clsx";
+
+type Tag = "a" | "button";
+
+interface BaseProps {
+  icon?: React.ReactNode;
+  tag: Tag;
+  classes?: {
+    root?: string;
+    item?: string;
+  };
+  gap?: string | number;
+}
+
+interface AnchorProps
+  extends BaseProps,
+    React.AnchorHTMLAttributes<HTMLAnchorElement> {
+  tag: "a";
+  href: React.AnchorHTMLAttributes<HTMLAnchorElement>["href"];
+}
+
+interface ButtonProps
+  extends BaseProps,
+    React.ButtonHTMLAttributes<HTMLButtonElement> {
+  tag: "button";
+}
+
+type Props = AnchorProps | ButtonProps;
+
+export default function AsideMenuItem({
+  icon,
+  tag: Tag,
+  classes,
+  children,
+  ...others
+}: Props) {
+  const Element = (
+    <>
+      {icon}
+      <span className="font-bold menu-item__text">{children}</span>
+    </>
+  );
+
+  const className = [
+    "flex flex-row items-center menu-item__link hover:text-baseColor transition duration-300",
+    {
+      "p-2 rounded-[100%] hover:bg-highlighting": !children,
+      "py-3": children,
+    },
+    classes?.item,
+  ];
+
+  return (
+    <li className={clsx(["text-menuItem", classes?.root])}>
+      {Tag === "a" ? (
+        <a
+          className={clsx(className)}
+          {...(others as React.AnchorHTMLAttributes<HTMLAnchorElement>)}
+        >
+          {Element}
+        </a>
+      ) : (
+        <button
+          className={clsx(className)}
+          {...(others as React.ButtonHTMLAttributes<HTMLButtonElement>)}
+        >
+          {Element}
+        </button>
+      )}
+    </li>
+  );
+}
